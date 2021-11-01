598b63712883b7c976da0733388dc2f062869b87
lbonastre <lucas.bonastre@pucarasec.com>
2021-11-01 00:19:12 +0000

    minor refactor

--- src/zuthaka/test_integration.py
+++ src/zuthaka/test_integration.py
@@ -1,14 +1,18 @@
 import asyncio
 import pytest
+from .backendapi.dtos import RequestDto, C2Dto
+from .backendapi.services.ClassHandlers import Covenant
+# from .backendapi.services.async_service import Service
 
-valid_dto_is_alive = {'url':'http://127.0.0.1:5000'}
+service = Service.get_service()
+covenant_dto = C2Dto(c2_type='covenant_integration', options={
+    'url': 'http://127.0.0.1:7443', 'user': 'pucara', 'password': 'pucara'})
+false_covenant_dto = C2Dto(c2_type='covenant_integration', options={
+    'url': 'http://127.0.0.1:7443', 'user': 'false', 'password': 'false'})
 
-"""
-namedTuple: 
-c2_dto = ()
-"""
-
-@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+@pytest.mark.parametrize("service, covenant_dto, expected", [
+    (service, covenant_dto, True), (service, false_covenant_dto, False)])
 @pytest.mark.asyncio
-async def test_is_alive(test_input, expected):
-    assert eval(test_input) == expected
+async def test_is_alive(service, covenant_dto, expected):
+    class_handler = Covenant.CovenantC2(covenant_dto.options)
+    assert await class_handler.is_alive()

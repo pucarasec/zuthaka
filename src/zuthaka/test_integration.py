c115a7dd6fc6801365c39cd8fb26ed788732d734
lbonastre <lucas.bonastre@pucarasec.com>
2021-11-05 16:26:07 +0000

    added  exceptions to is_alive method

--- src/zuthaka/test_integration.py
+++ src/zuthaka/test_integration.py
@@ -1,4 +1,3 @@
-import asyncio
 import pytest
 from .backendapi.dtos import RequestDto, C2Dto, ListenerDto, LauncherDto
 from .backendapi.dtos import ResponseDto
@@ -6,7 +5,9 @@ from .backendapi.services.ClassHandlers import Covenant
 import logging
 logger = logging.getLogger(__name__)
 
-covenant_dto = C2Dto(
+_handler = Covenant.CovenantC2
+
+c2_dto = C2Dto(
     c2_type='covenant_integration',
     options={
         'url': 'https://127.0.0.1:7443',
@@ -31,40 +32,58 @@ launcher_dto = LauncherDto(
         }
     )
 
-listener_internal_id = 0 
+listener_internal_id = 0
 
-# false_covenant_dto = C2Dto(c2_type='covenant_integration', options={
-#     'url': 'https://127.0.0.1:7443', 'username': 'false', 'password': 'false'})
-# @pytest.mark.parametrize(" covenant_dto, expected", [
-#   ( covenant_dto, True), ( false_covenant_dto, False)])
-# async def test_is_alive( covenant_dto, expected):
+false_c2_dto = C2Dto(c2_type='covenant_integration', options={
+    'url': 'https://127.0.0.1:7443', 'username': 'false', 'password': 'false'})
+
+invalid_c2_dto = C2Dto(c2_type='covenant_integration', options={
+    'url': 'https://127.0.0.1:7443', 'username': 'false', 'password': 'false'})
+
+
+@pytest.mark.asyncio
+async def test_is_alive_invalid_c2():
+    with pytest.raises(ConnectionError) as err:
+        class_handler = _handler(invalid_c2_dto.options)
+        dto = RequestDto(c2=invalid_c2_dto)
+        await class_handler.is_alive(dto)
+
+@pytest.mark.asyncio
+async def test_is_alive_false_creds():
+    with pytest.raises(ConnectionRefusedError) as err:
+        class_handler = _handler(false_c2_dto.options)
+        dto = RequestDto(c2=false_c2_dto)
+        await class_handler.is_alive(dto)
+
 
 @pytest.mark.asyncio
 async def test_is_alive():
-    class_handler = Covenant.CovenantC2(covenant_dto.options)
-    dto = RequestDto(c2=covenant_dto)
+    class_handler = _handler(c2_dto.options)
+    dto = RequestDto(c2=c2_dto)
     result = await class_handler.is_alive(dto)
     assert isinstance(result, ResponseDto)
     assert result.successful_transaction is True
 
+
 @pytest.mark.asyncio
 async def test_retrieve_agents():
-    class_handler = Covenant.CovenantC2(covenant_dto.options)
-    dto = RequestDto(c2=covenant_dto)
+    class_handler = _handler(c2_dto.options)
+    dto = RequestDto(c2=c2_dto)
     result = await class_handler.retrieve_agents(dto)
     assert isinstance(result, ResponseDto)
     assert result.successful_transaction is True
     assert result.agents is not None
     assert isinstance(result.agents, list)
 
+
 @pytest.mark.asyncio
 async def test_create_listener():
-    c2_handler = Covenant.CovenantC2(covenant_dto.options)
+    c2_handler = _handler(c2_dto.options)
     logger.debug('c2_handler: %r', c2_handler)
 
     listener_types = await c2_handler.get_listener_types()
     listener_handler = listener_types[listener_dto.listener_type]
-    dto = RequestDto(c2=covenant_dto, listener=listener_dto)
+    dto = RequestDto(c2=c2_dto, listener=listener_dto)
     result = await listener_handler.create_listener(listener_dto.options, dto)
     assert isinstance(result, ResponseDto)
     assert result.successful_transaction is True
@@ -73,9 +92,10 @@ async def test_create_listener():
     global listener_internal_id
     listener_internal_id = created_listener.listener_internal_id
 
+
 @pytest.mark.asyncio
 async def test_create_and_retrieve_launcher():
-    c2_handler = Covenant.CovenantC2(covenant_dto.options)
+    c2_handler = _handler(c2_dto.options)
     logger.debug('c2_handler: %r', c2_handler)
 
     global listener_internal_id
@@ -88,7 +108,7 @@ async def test_create_and_retrieve_launcher():
             }
         )
 
-    dto = RequestDto(c2=covenant_dto, listener=auxiliar_listener_dto, launcher=launcher_dto)
+    dto = RequestDto(c2=c2_dto, listener=auxiliar_listener_dto, launcher=launcher_dto)
     launcher_types = await c2_handler.get_launcher_types()
     launcher_handler = launcher_types[launcher_dto.launcher_type]
 
@@ -97,14 +117,15 @@ async def test_create_and_retrieve_launcher():
     assert response.successful_transaction is True
     assert response.created_launcher is not None
 
+
 @pytest.mark.asyncio
 async def test_delete_listener():
-    c2_handler = Covenant.CovenantC2(covenant_dto.options)
+    c2_handler = _handler(c2_dto.options)
     logger.debug('c2_handler: %r', c2_handler)
 
     listener_types = await c2_handler.get_listener_types()
     listener_handler = listener_types[listener_dto.listener_type]
-    dto = RequestDto(c2=covenant_dto, listener=listener_dto)
+    dto = RequestDto(c2=c2_dto, listener=listener_dto)
     global listener_internal_id
     response = await listener_handler.delete_listener(listener_internal_id, listener_dto.options, dto)
     assert isinstance(response, ResponseDto)

